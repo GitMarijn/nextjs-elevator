2175c1788104f329388fb4feb714f045e8fb19ed
Marijn <marijnvanloo@gmail.com>
2022-03-05 23:52:03 +0000

    chore: set up styled-components and basic file structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GenericPageWrapper } from "../src/components/common/layout/layout.styled";
 import ControlPanel from "../src/components/dedicated/ControlPanel/ControlPanel";
 import Elevator from "../src/components/dedicated/Elevator/Elevator";
@@ -8,29 +8,46 @@ import Elevator from "../src/components/dedicated/Elevator/Elevator";
 const Home: NextPage = () => {
   const [currentFloor, setCurrentFloor] = useState<number>(0);
   const [direction, setDirection] = useState<string | null>(null);
-  const [requestedFloors, setRequestedFloors] = useState<Array<number>>([]);
+  // const [requestedFloors, setRequestedFloors] = useState<Array<number>>([]);
   let floorArray: Array<number> = [];
 
   const summonElevator = (direction: string) => {
     setDirection(direction);
   };
+
   const handleFloorSelection = (floor: number) => {
     if (!direction)
       return alert("Please select a direction first (UP or DOWN)");
 
-    if (direction === "up") floorArray = [...floorArray, floor];
-    const sortedFloors = [...new Set(floorArray)].sort((a, b) => a - b);
-    sortedFloors.forEach((item) =>
-      setInterval(() => setCurrentFloor(item), 2000)
-    );
-    clearInterval();
-    // setInterval(() => setCurrentFloor(sortedFloors[0]), 3000);
-    console.log(sortedFloors);
+    if (!floorArray.includes(floor) && currentFloor !== floor) {
+      floorArray = [...floorArray, floor].sort((a, b) => a - b);
+
+      floorArray.forEach((item, i) => {
+        console.log(i);
+
+        setTimeout(() => setCurrentFloor(item), i * 1000 + 2000);
+      });
+      console.log("ARRAY:", floorArray);
+    }
+
+    // if (direction === "up") {
+    //   floorArray = [...floorArray, floor];
+
+    //   const sortedFloors = [...new Set(floorArray)].sort((a, b) => a - b);
+
+    //   sortedFloors.forEach((item) => {
+    //     const bla = setInterval(() => setCurrentFloor(item), 2000);
+    //   });
+
+    //   if (currentFloor === sortedFloors[0]) {
+    //     sortedFloors.shift();
+    //   }
+
+    //   console.log(sortedFloors);
+    // }
 
     if (direction === "down")
       floorArray = [...floorArray, floor].sort((a, b) => b - a);
-
-    console.log(currentFloor);
   };
 
   console.log("CurrentFloor:", currentFloor);
